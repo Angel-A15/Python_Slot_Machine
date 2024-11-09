c534b25fe8781a20da8a91d478cbcd792318d30c
Angel-A15 <aasupertwix15@gmail.com>
2024-11-09 10:39:13 -0600

    finished functionality to add credits, working on functionality to add lines

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,7 @@
+MAX_LINES = 3
 
-# Function will allow the user to interact with in order to add credits
+
+# Function will allow the user to interact with the interface order to add credits
 def deposit():
     while True:
         amount = input("What would like to deposit? $")
@@ -13,5 +15,25 @@ def deposit():
             print("Please enter a number.")
     return amount
 
-deposit()
+# Function will allow the user to interact with the interface order to add number of lines
+def get_number_lines():
+    while True:
+        lines = input("Enter the number of lines you want to bet on (1-" + str(MAX_LINES) + "). ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Please enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+    return lines
+
+
+def main():
+    balance = deposit()
+    lines = get_number_lines()
+    print(balance, lines)
+
+main()
 
