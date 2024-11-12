a6d251a6c217188fa7d846a5b5a77479247e0b01
Angel-A15 <aasupertwix15@gmail.com>
2024-11-12 03:16:26 -0600

    slot machine columns and rows display from left to right now, working on user interaction to continue betting

--- main.py
+++ main.py
@@ -35,13 +35,17 @@ def get_slot_machine_spin(rows, cols, symbols):
     
     return columns
 
+# Function will generate columns and rows to be displayed across(left to right)
 def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) -1:
-                print(column[row], "|")
+                print(column[row], end = " | ")
             else:
-                print(column[row])
+                print(column[row], end = "")
+
+        print()
+        
 
 
 
